5ab83d81680d64843ad01911afe9a95573f72f79
Gaurika Bhatt <143697449+Gaurikabhatt27@users.noreply.github.com>
2025-11-27 19:37:27 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12367
+let accountEmail = "preety@outlook.com"
+var accountPassword = "12453"
+accountCity = "Jaipur"
+let accountStatus;
+
+console.log(accountId)
+
+// accountId = 3
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountStatus])
+
