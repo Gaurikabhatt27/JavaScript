5a433d654829f64b0b97a94c6ed922df9f821978
Gaurika Bhatt <143697449+Gaurikabhatt27@users.noreply.github.com>
2025-11-30 18:45:29 +0000

    datatypes in JS

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,11 +2,14 @@ const accountId = 12367
 let accountEmail = "preety@outlook.com"
 var accountPassword = "12453"
 accountCity = "Jaipur"
-let accountStatus;
+let accountStatus = "Open";
 
+console.log(accountPassword)
 console.log(accountId)
 
 // accountId = 3
+var accountPassword = "234"
+accountStatus = "Closed"
 
 console.log(accountId);
 console.table([accountId, accountEmail, accountPassword, accountCity, accountStatus])
