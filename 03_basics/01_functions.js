dfe1d9e2c5fd7a9b7a92c298e9904aee3f7230d1
Gaurika Bhatt <143697449+Gaurikabhatt27@users.noreply.github.com>
2026-01-03 17:19:52 +0000

    func continuation

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,4 +36,35 @@ function loginUserMessage(username = "Sam"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("Gaurika"))
+// console.log(loginUserMessage("Gaurika"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "Gaurika",
+    price: 399
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "Sam",
+    price: 499
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([2, 5, 8]));
