13c7ff5b5b5f003446a199ca07008fc9353ba77d
Gaurika Bhatt <143697449+Gaurikabhatt27@users.noreply.github.com>
2026-01-17 17:55:06 +0000

    iife in js

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+// Prevents from the problem of global scope
+// To remove the pollution of global scope, we use iife
+
+(function chai(){
+    // named iife
+    console.log(`DB connected`);
+})();
+
+( (name) => {
+    // unnamed iife
+    console.log(`DB connected TWO ${name}`);
+})('Gaurika')
